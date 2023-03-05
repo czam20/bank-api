66399625ad38ff555ae5e1e44962ea1a45c68fd5
cczam20 <ccza2010@gmail.com>
2023-03-04 22:07:39 -0400

    Add: db transactions

--- bank/api.py
+++ bank/api.py
@@ -1,3 +1,4 @@
+from django.db import transaction
 from rest_framework import viewsets, permissions, status, mixins
 from rest_framework.decorators import action
 from rest_framework.response import Response
@@ -45,33 +46,46 @@ class TransactionViewSet(viewsets.ModelViewSet):
         serializer = self.serializer_class(queryset, many=True)
         return Response({'transactions': serializer.data}, status=status.HTTP_200_OK)
 
+    @transaction.atomic
     @action(detail=True, methods=['post'])
     def make_deposit_or_withdrawal(self, request):
         requestData = request.data
-
         accountId = requestData.pop('account')
         data = {
             **requestData,
+            'amount': requestData['amount']*(-1) if requestData['transaction_type'] == 'Withdrawal' else requestData['amount'],
             "accountsId": [accountId]
         }
-
         serializer = self.serializer_class(data=data)
-        if serializer.is_valid():
-            # update account amount
-            account = Account.objects.filter(id=accountId).first()
-            newAmount = modifyAccountAmount(
-                account.amount, requestData['amount'], requestData['transaction_type'])
-            accountSerializer = AccountSerializer(
-                account, data={'amount': newAmount}, partial=True)
+        account = Account.objects.select_for_update().filter(id=accountId).first()
 
-            if accountSerializer.is_valid():
+        with transaction.atomic():
+            if serializer.is_valid():
+                # update account amount 
+                newAmount = modifyAccountAmount(
+                    account.amount, requestData['amount'], requestData['transaction_type'])
+                account.amount = newAmount
+                account.save()
                 serializer.save()
-                accountSerializer.save()
                 return Response({'message': 'Transaction completed successfully!', 'transaction': serializer.data}, status=status.HTTP_201_CREATED)
-            return Response(accountSerializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    # def make_deposit_or_withdrawal(self, request):
+    #     requestData = request.data
 
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    #     accountId = requestData.pop('account')
+    #     data = {
+    #         **requestData,
+    #         "accountsId": [accountId]
+    #     }
 
+    #     serializer = self.serializer_class(data=data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response({'message': 'Transaction completed successfully!', 'transaction': serializer.data}, status=status.HTTP_201_CREATED)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    @transaction.atomic
     @action(detail=True, methods=['post'])
     def make_transfer(self, request):
         requestData = request.data
@@ -82,29 +96,40 @@ class TransactionViewSet(viewsets.ModelViewSet):
             "accountsId": [senderAccountId, receiverAccountId],
             "transaction_type": "Transfer"
         }
-
+        senderAccount = Account.objects.filter(id=senderAccountId).select_for_update().first()
+        receiverAccount = Account.objects.select_for_update().filter(id=receiverAccountId).first()
+        
         serializer = self.serializer_class(data=data)
-        if serializer.is_valid():
-            # update accounts amount
-            senderAccount = Account.objects.filter(id=senderAccountId).first()
-            newSenderAccountAmount = modifyAccountAmount(
-                senderAccount.amount, requestData['amount'], 'Withdrawal')
-            senderAccountSerializer = AccountSerializer(
-                senderAccount, data={'amount': newSenderAccountAmount}, partial=True)
+        with transaction.atomic():
+            if serializer.is_valid():
+                # update accounts amount
+                newSenderAccountAmount = modifyAccountAmount(
+                    senderAccount.amount, requestData['amount'], 'Withdrawal')
+                senderAccount.amount = newSenderAccountAmount
+                senderAccount.save()
 
-            receiverAccount = Account.objects.filter(
-                id=receiverAccountId).first()
-            newReceiverAccountAmount = modifyAccountAmount(
-                receiverAccount.amount, requestData['amount'], 'Deposit')
-            receiverAccountSerializer = AccountSerializer(
-                receiverAccount, data={'amount': newReceiverAccountAmount}, partial=True)
+                newReceiverAccountAmount = modifyAccountAmount(
+                    receiverAccount.amount, requestData['amount'], 'Deposit')
+                receiverAccount.amount = newReceiverAccountAmount
+                receiverAccount.save()
+                serializer.save()
+                return Response({'message': 'Transfer completed successfully!', 'transaction': serializer.data}, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    # def make_transfer(self, request):
+    #     requestData = request.data
+    #     senderAccountId = requestData.pop('sender_account')
+    #     receiverAccountId = requestData.pop('receiver_account')
+    #     data = {
+    #         **requestData,
+    #         "accountsId": [senderAccountId, receiverAccountId],
+    #         "transaction_type": "Transfer"
+    #     }
 
-            if senderAccountSerializer.is_valid():
-                if receiverAccountSerializer.is_valid():
-                    serializer.save()
-                    senderAccountSerializer.save()
-                    receiverAccountSerializer.save()
-                    return Response({'message': 'Transfer completed successfully!', 'transaction': serializer.data}, status=status.HTTP_201_CREATED)
-                return Response(receiverAccountSerializer.errors, status=status.HTTP_400_BAD_REQUEST)
-            return Response(senderAccountSerializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    #     serializer = self.serializer_class(data=data)
+    #     if serializer.is_valid():
+
+    #         serializer.save()
+
+    #         return Response({'message': 'Transfer completed successfully!', 'transaction': serializer.data}, status=status.HTTP_201_CREATED)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
